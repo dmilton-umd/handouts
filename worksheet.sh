3ee40257fffd55a14ea77a343de83c77595d8554
dmilton-umd <dmilton@umd.edu>
2018-10-30 12:46:51 -0400

    add worksheet update

--- worksheet.sh
+++ worksheet.sh
@@ -1,9 +1,11 @@
 # configure git
 
-git config --global user.name ...
-git config --global user.email ...
+git config --global user.name dmilton-umd
+git config --global user.email dmilton@umd.edu
 git commit --no-edit --amend --reset-author
 
 # Link your local repository to the origin repository on GitHub, by
 # copying the code shown on your GitHub repo under the heading:
 # "…or push an existing repository from the command line"
+git remote add origin https://github.com/dmilton-umd/handoudts.git
+git push -u origin master
